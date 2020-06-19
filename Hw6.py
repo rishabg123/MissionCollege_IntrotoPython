254e2d0ed6f1dc1ddecbfe68b65cc2e8d1c01798
Rishab Gupta <rishabg2004@gmail.com>
2020-06-18 18:55:50 -0700

    Midterm update

--- Hw6.py
+++ Hw6.py
@@ -1,58 +1,73 @@
-# 6.9
-
-def footToMeter(foot):
-    meter = foot * 0.305
-    print(meter)
-def metrerTofFoot(meter):
-    foot = meter/0.35
-    print(foot)
-print(footToMeter(eval(input("Enter an amount of feet to convert: "))))
-print(metrerTofFoot(eval(input("Enter an amount of meters to convert: "))))
-
-#6.18
-from random import randint
-
-def printMatrix(n):
-    for i in range(n):
-        for j in range(n):
-            print(randint(0, 1), end="")
-        print("")
-number = input("Choose your number: ")
-printMatrix(int(number))
+# # 6.9
+#
+# def footToMeter(foot):
+#     meter = foot * 0.305
+#     return meter
+# def metrerTofFoot(meter):
+#     foot = meter/0.35
+#     return foot
+#
+# print(footToMeter(eval(input("Enter an amount of feet to convert: "))))
+# print(metrerTofFoot(eval(input("Enter an amount of meters to convert: "))))
+#
+# #6.18
+# from random import randint
+#
+# def printMatrix(n):
+#     for i in range(n):
+#         for j in range(n):
+#             print(randint(0, 1), end="")
+#         print("")
+# number = input("Choose your number: ")
+# printMatrix(int(number))
 
 # 6.6
-
+n = int(input("Enter a value for n: "))
 def displayPattern(n):
-    for num in range(n):
-        for i in range(num):
-            print(num, end=" ")
-        print(" ")
-print(displayPattern(eval(input("Enter a number: "))))
+    for i in range(1, n + 1):
+        for j in range(n, 0, -1):
+            if j > i:
+                print("  ", end='')
+            else:
+                print(j, end=" ")
+
+        print("")
+
+
+displayPattern(n)
 
 # 6.10
 
-def isPrime(number):
-    divisor = 2
-    while divisor <= number / 2:
-        if number % divisor == 0:
-            return False
-        divisor += 1
-    return True
-
-for i in range(10000):
-    if isPrime(i) == True:
-        print(i)
-
-# 6.39
+# def isPrime(number):
+#     divisor = 2
+#     while divisor <= number / 2:
+#         if number % divisor == 0:
+#             return False
+#         divisor += 1
+#     return True
+#
+# for i in range(10000):
+#     if isPrime(i) == True:
+#         print(i)
+#
+# # 6.39
 
 import turtle
 import tkinter
+def draw_star():
+    print("Input an amount of sides you want for the star")
+    sides = int(input())
+    angle = 120
 
-turtle = turtle.Turtle()
 
-count = 1
-while count <= 5:
-    turtle.forward(300)
-    turtle.right(144)
-    count = count + 1
+    for side in range(sides):
+        turtle.forward(100)
+        turtle.right(angle)
+        turtle.forward(100)
+        turtle.right(360/sides - angle)
+
+
+draw_star()
+
 tkinter.mainloop()
+
