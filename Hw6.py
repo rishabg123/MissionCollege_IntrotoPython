6cc4b2af79a1cb036031fd727856858a836633c9
Rishab Gupta <rishabg2004@gmail.com>
2020-06-22 17:31:56 -0700

    Finishing Hw #6

--- Hw6.py
+++ Hw6.py
@@ -1,25 +1,25 @@
-# # 6.9
-#
-# def footToMeter(foot):
-#     meter = foot * 0.305
-#     return meter
-# def metrerTofFoot(meter):
-#     foot = meter/0.35
-#     return foot
-#
-# print(footToMeter(eval(input("Enter an amount of feet to convert: "))))
-# print(metrerTofFoot(eval(input("Enter an amount of meters to convert: "))))
-#
-# #6.18
-# from random import randint
-#
-# def printMatrix(n):
-#     for i in range(n):
-#         for j in range(n):
-#             print(randint(0, 1), end="")
-#         print("")
-# number = input("Choose your number: ")
-# printMatrix(int(number))
+# 6.9
+
+def footToMeter(foot):
+    meter = foot * 0.305
+    return meter
+def metrerTofFoot(meter):
+    foot = meter/0.35
+    return foot
+
+print(footToMeter(eval(input("Enter an amount of feet to convert: "))))
+print(metrerTofFoot(eval(input("Enter an amount of meters to convert: "))))
+
+#6.18
+from random import randint
+
+def printMatrix(n):
+    for i in range(n):
+        for j in range(n):
+            print(randint(0, 1), end="")
+        print("")
+number = input("Choose your number: ")
+printMatrix(int(number))
 
 # 6.6
 n = int(input("Enter a value for n: "))
@@ -38,19 +38,19 @@ displayPattern(n)
 
 # 6.10
 
-# def isPrime(number):
-#     divisor = 2
-#     while divisor <= number / 2:
-#         if number % divisor == 0:
-#             return False
-#         divisor += 1
-#     return True
-#
-# for i in range(10000):
-#     if isPrime(i) == True:
-#         print(i)
-#
-# # 6.39
+def isPrime(number):
+    divisor = 2
+    while divisor <= number / 2:
+        if number % divisor == 0:
+            return False
+        divisor += 1
+    return True
+
+for i in range(10000):
+    if isPrime(i) == True:
+        print(i)
+
+# 6.39
 
 import turtle
 import tkinter
