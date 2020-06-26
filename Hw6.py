10188524ad7a2ed007b129dbeda5c5b74c0c7c7e
Rishab Gupta <rishabg2004@gmail.com>
2020-06-25 18:29:24 -0700

    Finishing Hw6

--- Hw6.py
+++ Hw6.py
@@ -3,12 +3,12 @@
 def footToMeter(foot):
     meter = foot * 0.305
     return meter
-def metrerTofFoot(meter):
-    foot = meter/0.35
+def meterToFoot(meter):
+    foot = meter/0.305
     return foot
 
 print(footToMeter(eval(input("Enter an amount of feet to convert: "))))
-print(metrerTofFoot(eval(input("Enter an amount of meters to convert: "))))
+print(meterToFoot(eval(input("Enter an amount of meters to convert: "))))
 
 #6.18
 from random import randint
@@ -16,7 +16,8 @@ from random import randint
 def printMatrix(n):
     for i in range(n):
         for j in range(n):
-            print(randint(0, 1), end="")
+            print(randint(0, 1), end="    ")
+        print("")
         print("")
 number = input("Choose your number: ")
 printMatrix(int(number))
@@ -36,38 +37,52 @@ def displayPattern(n):
 
 displayPattern(n)
 
+print()
+print()
+
 # 6.10
+def isPrime(n):
+   if n <= 1:
+       return False
 
-def isPrime(number):
-    divisor = 2
-    while divisor <= number / 2:
-        if number % divisor == 0:
-            return False
-        divisor += 1
-    return True
 
-for i in range(10000):
-    if isPrime(i) == True:
-        print(i)
+   for i in range(2, n):
+       if n % i == 0:
+           return False
+
+   return True
+
+def printPrime(n):
+   printedPrimecount = 0
+   for i in range(2, n + 1):
+       if isPrime(i):
+           print(i, end=' ')
+           printedPrimecount += 1
+           if printedPrimecount % 20 == 0:
+               print()
+
+
+if __name__ == "__main__":
+   n = 10000
+   printPrime(n)
+   print()
+   print()
+   print("There are 1229 prime numbers")
+
 
 # 6.39
 
 import turtle
-import tkinter
-def draw_star():
-    print("Input an amount of sides you want for the star")
-    sides = int(input())
-    angle = 120
+def drawStar(x1, x2, y1, y2, x3, y3, x4, y4, x5, y5, x6, y6):
+    turtle.penup()
+    turtle.goto(x1, y1)
+    turtle.pendown()
+    turtle.goto(x2, y2)
+    turtle.goto(x3, y3)
+    turtle.goto(x4, y4)
+    turtle.goto(x5, y5)
+    turtle.goto(x6, y6)
 
-
-    for side in range(sides):
-        turtle.forward(100)
-        turtle.right(angle)
-        turtle.forward(100)
-        turtle.right(360/sides - angle)
-
-
-draw_star()
-
-tkinter.mainloop()
-
+turtle.showturtle()
+drawStar(-111, -68, -50, 122.65, -11.8, -68, -150, 50, 30, 50, -111, -50)
+turtle.done()
